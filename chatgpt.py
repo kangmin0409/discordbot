7ac07712ba0c5fce7bb3ef224e84f7d64df679c4
kangmin <93463938+kangmin0409@users.noreply.github.com>
2023-07-06 22:36:48 +0900

    openai api

---
+++ chatgpt.py
@@ -0,0 +1,14 @@
+import openai
+
+# API 키 설정
+openai.api_key = '<youtapikey>'
+
+# ChatGPT에 요청 보내기
+response = openai.Completion.create(
+  engine='text-davinci-003',  # ChatGPT 모델 선택
+  prompt='What is the meaning of life?',  # 대화 시작 문장
+  max_tokens=50  # 생성할 최대 토큰 수
+)
+
+# 응답 출력
+print(response.choices[0].text.strip())
